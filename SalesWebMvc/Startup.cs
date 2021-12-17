3116b5a8c2e79fbd8603991c5f04bab9589c98d5
phvds <paulocraazy@gmail.com>
2021-12-16 21:06:41 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -41,6 +41,7 @@ namespace SalesWebMvc
 					builder.MigrationsAssembly("SalesWebMvc")));
 
 			services.AddScoped<SeedingService>();
+			services.AddScoped<SeedingService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
