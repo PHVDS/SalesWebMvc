e20b068e041d8f556a4be64002342baac300b576
phvds <paulocraazy@gmail.com>
2021-12-22 17:10:17 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,21 +8,29 @@ namespace SalesWebMvc.Models
 	public class Seller
 	{
 		public int Id { get; set; }
+
+		[Required(ErrorMessage = "{0} obrigatorio")]
+		[StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do {0} entre {2} e {1} caracteres")]
 		public string Name { get; set; }
-		
+
+		[Required(ErrorMessage = "{0} obrigatorio")]
 		[DataType(DataType.EmailAddress)]
+		[EmailAddress(ErrorMessage = "Email invalido")]
 		public string Email { get; set; }
 
+		[Required(ErrorMessage = "{0} obrigatorio")]
 		[Display(Name="Birth Date")]
 		[DataType(DataType.Date)]
 		[DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
 		public DateTime BirthDate { get; set; }
 
-
+		[Required(ErrorMessage = "{0} obrigatorio")]
+		[Range(100.0, 50000.0, ErrorMessage = "{0} deve ser de {1} ate {2}")]
 		[Display(Name="Base Salary")]
 		[DisplayFormat(DataFormatString ="{0:F2}")]
 		public double BaseSalary { get; set; }
 		
+
 		public Department Department { get; set; }
 		public int DepartmentId { get; set; }
 		public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
